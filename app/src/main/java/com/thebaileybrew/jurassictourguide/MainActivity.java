45baa64b49c61b77f7dc929128cbe2d475f129e3
matthew.bailey@hthackney.com <Brycen624>
2018-06-08 14:17:19 -0400

    Initial commit

---
+++ app/src/main/java/com/thebaileybrew/jurassictourguide/MainActivity.java
@@ -0,0 +1,35 @@
+package com.thebaileybrew.jurassictourguide;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // https://antonioleiva.com/navigation-view/
+    // https://github.com/chrisbanes/cheesesquare/blob/master/app/src/main/java/com/support/android/designlibdemo/CheeseListFragment.java
+    // https://github.com/antoniolg/MaterializeYourApp
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
